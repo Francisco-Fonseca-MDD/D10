30317f8cb4b1cb5928a4ca5c28b61e9599a70385
Francisco Fonseca <54849537+creeperbuster69@users.noreply.github.com>
2021-08-02 15:43:37 +0100

    Add files via upload
    Rolls are now colour coded

--- D10.py
+++ D10.py
@@ -11,14 +11,38 @@ Roll = np.random.randint(1,11,n)
 
 Successes = AutoSucc
 
-for i in Roll:
-    if i>=TargetNumber:
-        Successes = Successes+1
-        if i >= DoubleThresh :
-            Successes = Successes+1
 
-st.table(sorted(Roll))
-st.write('Number of successes is ' + str(Successes))
+
+st.write('<p style="color:DodgerBlue;">Your Roll is: </p>',unsafe_allow_html=True)
+NoSucCounter=0
+DoubleCounter=0
+for i in sorted(Roll):
+    if i>=DoubleThresh:
+        Successes = Successes+2
+        #st.write(i,': Double Success')
+    elif i >= TargetNumber:
+        Successes = Successes+1
+        DoubleCounter=DoubleCounter+1
+        #st.write(i,': Success')
+    else:
+        #st.write(i,': No Success')
+        NoSucCounter = NoSucCounter+1
+
+StringRoll = str(sorted(Roll))
+NoSuc = f"<p style=\"color:#ff0000;\">{StringRoll[0:NoSucCounter*3+1]} </p>"
+Suc   = f"<p style=\"color:#009900;\"> {StringRoll[NoSucCounter*3+1:(DoubleCounter+NoSucCounter)*3+1]} </p>"
+DoubleSuc   = f"<p style=\"color:#3366ff;\"> {StringRoll[(DoubleCounter+NoSucCounter)*3+1:]} </p>"
+
+RollDisp = NoSuc + Suc + DoubleSuc
+st.write(RollDisp,unsafe_allow_html=True)
+
+#st.write('length: ',len(StringRoll))
+#st.write('Counter 1: ', NoSucCounter)
+#st.write('Counter 2: ', NoSucCounter+DoubleCounter)
+#st.write(StringRoll)
+
+st.text(StringRoll)
+st.write('Number of successes is ' + str(Successes) )
 
 if Successes == 0:
     for i in Roll:
